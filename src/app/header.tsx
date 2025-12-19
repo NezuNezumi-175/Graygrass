95680f232a789750a5c61d38c49e332899d2b45b
Tawo12Ruletree <kazuki.ruleetree@icloud.com>
2025-12-19 17:29:02 +0900

    header add color

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -7,10 +7,14 @@ export default function Nav() {
   const pathname = usePathname()
 
   const linkClass = (href: string) =>
-    pathname === href ? 'text-red-500' : ''
+    `px-5 py-2 rounded-full transition
+     ${pathname === href
+       ? 'bg-red-400 text-yellow-400 hover:bg-red-500'
+       : 'bg-gray-300 text-gray-400 hover:bg-gray-400 hover:text-gray-600'
+     }`
 
   return (
-    <header className="fixed bottom-0 left-0 w-full border-t p-4 flex gap-4 bg-white justify-center">
+    <header className="fixed bottom-0 left-0 w-full border-t p-4 flex gap-12 bg-white justify-center font-extrabold">
       <Link href="/" className={linkClass('/')}>Home</Link>
       <Link href="/feed" className={linkClass('/feed')}>Feed</Link>
       <Link href="/capture" className={linkClass('/capture')}>Capture</Link>
