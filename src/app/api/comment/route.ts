4e35480418b9319ab573614baaf1b4f4aba0f881
Nezumi <shiba.2021@outlook.jp>
2025-12-20 15:45:14 +0900

    修正版

--- src/app/api/comment/route.ts
+++ src/app/api/comment/route.ts
@@ -1,10 +1,10 @@
 import { createClient } from '@/lib/supabase/server'
 
-export async function Follow(follow_id: string,follower_id: string) {
+export async function Comment(comment: string,follower_id: string) {
     const supabase = await createClient()
     const { data, error } = await supabase
-        .from('follows')
+        .from('comments')
         .insert([
-            { follow_id: follow_id, follower_id: follower_id }
+            { text: comment, follower_id: follower_id }
         ])
 }
