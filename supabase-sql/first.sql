90d73e1bd16d74b66f2818c39532c93334514aab
Koutako2024 <koutako2021@yahoo.co.jp>
2025-12-20 21:45:31 +0900

    doc: create comments table on supabase.

--- supabase-sql/first.sql
+++ supabase-sql/first.sql
@@ -1,4 +1,4 @@
-5622-- profiles（auth.users と紐付け）
+-- profiles（auth.users と紐付け）
 create table if not exists profiles (
   id uuid primary key references auth.users (id) on delete cascade,
   email text unique,
