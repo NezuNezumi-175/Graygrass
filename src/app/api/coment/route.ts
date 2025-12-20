8d1b60a8a50a7c8f98faf60f2399714918f75ca3
Nezumi <shiba.2021@outlook.jp>
2025-12-20 15:29:14 +0900

    修正

--- src/app/api/coment/route.ts
+++ src/app/api/coment/route.ts
@@ -1,10 +1,1 @@
 import { createClient } from '@/lib/supabase/server'
-
-export async function Comment(text:string,submission_id:string) {
-    const supabase = createClient()
-    const { data, error } = await supabase
-        .from('comments')
-        .insert([
-            { text: text, submission_id: submission_id }
-        ])
-}
